30a6b3f5a2cc23088fd9bd248b3109d60c5871a0
Helloyunho <yunho050840@gmail.com>
2021-10-16 19:44:33 +0900

    🍻 feat: add somewhat working js repl

--- Package.swift
+++ Package.swift
@@ -1,15 +1,45 @@
 // swift-tools-version:5.5
 import PackageDescription
 
+let deps: [Target.Dependency] = [
+    "SwiftCLI"
+]
+
 let package = Package(
     name: "Oned",
+    products: [
+        .executable(name: "Oned", targets: ["Oned"])
+    ],
     dependencies: [
+        .package(url: "https://github.com/jakeheis/SwiftCLI", .upToNextMajor(from: "6.0.3"))
     ],
     targets: [
         .executableTarget(
             name: "Oned",
-            dependencies: []),
+            dependencies: deps
+        ),
         .testTarget(
             name: "OnedTests",
-            dependencies: ["Oned"]),
-    ])
+            dependencies: ["Oned"])
+    ]
+)
+
+#if os(Linux)
+deps.append(
+    .target(name: "JavaScriptCore", condition: .when(platforms: [.linux]))
+)
+package.targets = [
+    .executableTarget(
+        name: "Oned",
+        dependencies: deps
+    )
+    .testTarget(
+        name: "OnedTests",
+        dependencies: ["Oned"]),
+    .systemLibrary(
+        name: "JavaScriptCore",
+        pkgConfig: "javascriptcoregtk-4.0",
+        providers: [.apt(["libjavascriptcoregtk-4.0-dev"])]
+    )
+]
+#endif
--- Sources/Oned/main.swift
+++ Sources/Oned/main.swift
@@ -1,1 +1,22 @@
-print("Hello, world!")
+import JavaScriptCore
+import SwiftCLI
+
+let context = JSContext()
+
+struct ConsoleHandlers {
+    static let log: @convention(block) (JSValue) -> Void = { value in
+        if value.isUndefined {
+            print()
+        } else {
+            print(value)
+        }
+    }
+}
+
+context?.objectForKeyedSubscript("console")?.setValue(ConsoleHandlers.log, forProperty: "log")
+
+while true {
+    let input = Input.readLine(prompt: "> ")
+    let value = context?.evaluateScript(input)
+    print(value ?? "undefined")
+}
